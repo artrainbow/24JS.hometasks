8fefdd7def3f4aba58d141ce5ce11e0eb3ee526a
artrainbow <artrainbow@gmail.com>
2017-05-31 17:33:41 +0300

    finished

--- 3105/js/main.js
+++ 3105/js/main.js
@@ -1,4 +1,5 @@
-// var 1
+//---- TASK 1
+//-- var 1
 
 var i = 0;
 function func (i) {
@@ -11,6 +12,48 @@ function func (i) {
 	}
 };
 
-//var 2
+//-- var 2
 
-function funcClosure()
+var a = 0;
+function func (b) {
+	if (a <= Infinity) {
+		a += b;
+		return a;
+	} else {
+		return 0;
+	}
+};
+
+
+//-- var 3
+
+ function funcClosure() {
+	var y = 0;
+	return function (x){
+		if(x <= Infinity) {
+
+			return y +=x; 
+		} else {
+			return y = 0;
+		}
+	}
+}
+
+var closure = funcClosure();
+
+
+//---- TASK 2
+
+!function z (f){
+	var obj = {
+		name : 'Вася'
+	}
+	function f(obj) {
+		obj.age = 5;
+		return obj;
+	};
+	f(obj);
+	console.log(obj);
+}()
+
+
