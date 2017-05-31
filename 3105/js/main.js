43ea011812c5af21efd85ae0afd99e5095d9e0bb
artrainbow <artrainbow@gmail.com>
2017-05-31 16:15:22 +0300

    var1

--- 3105/js/main.js
+++ 3105/js/main.js
@@ -1,2 +1,16 @@
-alert ('hello');
+// var 1
 
+var i = 0;
+function func (i) {
+	if (i <= Infinity) {
+
+		window.i += i;
+		return window.i;
+	} else {
+		return 0;
+	}
+};
+
+//var 2
+
+function funcClosure()
