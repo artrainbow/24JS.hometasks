4089f8bf82f13142de9bfcaa91146c76263c54d2
artrainbow <artrainbow@gmail.com>
2017-06-15 11:18:08 +0300

    new

---
+++ 1506/ht1.js
@@ -0,0 +1,12 @@
+var User = {
+	getName: function(arg1) {
+		console.log (arg1);
+		return this;
+	},
+	getCode: function(arg2) {
+		console.log (arg2);
+		return this;
+	}
+}
+
+User.getName('Hello').getCode('World');
---
+++ 1506/ht2.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+	res.end('ok');
+});
+
+server.listen(8080);
+
