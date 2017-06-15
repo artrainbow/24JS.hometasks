3da70a0b4283430c6e3a27dbf5c2104a5c2f1746
artrainbow <artrainbow@gmail.com>
2017-06-15 13:39:50 +0300

    task 2
    finished

--- 1506/ht2.js
+++ 1506/ht2.js
@@ -3,6 +3,16 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
 	res.end('ok');
 });
-
+server.once('connection', function() {
+	console.log('Connect...');
+});
 server.listen(8080);
+server.once('listening', function() {
+	console.log('Listen port', server.address().port);
+});
+server.once('request', function(req, res) {
+	console.log('Method: ', http.request().method);
+	console.log('URL: ', http.request()._headers.host);
+	console.log('Status code: ', res.statusCode);
+});
 
