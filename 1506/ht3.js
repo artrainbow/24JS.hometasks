ba1d62823bb38e3f5d4c15eaa3a0c99049251c60
artrainbow <artrainbow@gmail.com>
2017-06-20 12:30:24 +0300

    ht3 is finished

--- 1506/ht3.js
+++ 1506/ht3.js
@@ -1,18 +1,29 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
 var http = require('http');
 var server = http.createServer().listen(8080);
-var users = [];
+var users = {};
+var userCounter = 0;
+
 server.on('request', function(req, res){
 
-    /*if( res._header != a ) {
-        //console.log('hey');
-        users.push(res._header);
+    // fixing chrome request favicon
+    if (req.url === '/favicon.ico') {
+        res.end();
+        return;
     }
 
-    console.log(users);
-    var a = res._header;*/
-
+    var date = new Date();
+    var currentTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    var Id = ++userCounter;
+    users['user_' + Id] = currentTime;
     res.end('ok');
-    console.log(res._header);
+
+    // logger
+    console.log('connection log', users);
+
+    myEmitter.addListener('logIn', function (){});
+    myEmitter.emit('logIn', console.log('user_' + Id + ' connected...'));
 });
 
 
